06ec448e281d4f63e625a8c38d65cc45d7782793
Yuichi Yazaki <visualizingjp@gmail.com>
2025-06-09 16:28:13 +0900

    moduleNameの修正

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import nodeResolve from "rollup-plugin-node-resolve";
 
 export default {
   entry: 'index.js',
-  moduleName: 'topogram',
+  moduleName: 'd3',
   format: 'umd',
   dest: 'build/topogram.js',
   plugins: [
